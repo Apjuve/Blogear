bf5c69bfdfd1e26fb01133497c052ab66c990bca
Apjuve <abiponce3@hotmail.com>
2022-05-06 22:24:35 -0400

    added route to add and delete blogs

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
+const blogRoutes = require('./blogRoutes');
+
+
 router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
+router.use('/blogs', blogRoutes);
 
 
 module.exports = router;
