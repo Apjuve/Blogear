a70150a268633b151a9b694d0245524f2ef9f8ea
Apjuve <abiponce3@hotmail.com>
2022-05-08 14:23:13 -0400

    created blog-pst.js

---
+++ public/js/blog-post.js
@@ -0,0 +1,23 @@
+const blogFormHandler = async (event) => {
+    event.preventDefault();
+
+    const blog_name = document.querySelector('#blog-name').value;
+    const description = document.querySelector('#description').value;
+
+    if (blog_name && description) {
+        const response = await fetch('/api/blogs', {
+            method: 'POST',
+            body: JSON.stringify({ blog_name, description }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        console.log(response);
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#blog-form')
+document.addEventListener('submit', blogFormHandler);
