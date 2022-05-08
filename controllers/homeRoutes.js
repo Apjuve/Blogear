0578631bb586ab5f8bc904ed9f87d5c72ffd987e
Apjuve <abiponce3@hotmail.com>
2022-05-08 10:37:58 -0400

    updated  login.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,6 +62,18 @@ router.get('/blog/:id', async (req, res) => {
 });
 
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('homepage');
+      return;
+    }
+
+    res.render('login');
+  });
+
+
+
 
 
 module.exports = router;
