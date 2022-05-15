ce1166d92d16402f1013661d53dc173e33f2195d
Apjuve <abiponce3@hotmail.com>
2022-05-14 22:24:06 -0400

    creaated signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.signup-form')
+document.addEventListener('submit', signupFormHandler);
